a550b9f1eb36038e9eb3f09dc2f5aa2dfd02e073
pulnara <magnes.agnieszka@gmail.com>
2021-12-13 00:32:34 +0100

    Change type of roll and pitch to degrees instead of radians

--- app/src/main/java/com/example/androidfalldetection/AccelerometerData.kt
+++ app/src/main/java/com/example/androidfalldetection/AccelerometerData.kt
@@ -2,15 +2,16 @@ package com.example.androidfalldetection
 
 import kotlin.math.atan
 import kotlin.math.sqrt
+import kotlin.math.PI
 
 class AccelerometerData(val x: Float, val y: Float, val z: Float) {
 
     fun countRoll(): Float {
-        return atan(y / sqrt( x * x + z * z  ))
+        return atan(y / sqrt( x * x + z * z  )) * 180 / PI.toFloat()
     }
 
     fun countPitch(): Float {
-        return atan(x / sqrt( y * y + z * z  ))
+        return atan(x / sqrt( y * y + z * z  )) * 180 / PI.toFloat()
     }
 
     fun countAlpha(): Float {
